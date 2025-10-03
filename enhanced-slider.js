aa10380d2fcb97b1a720ea130890a96e49a0122f
Alexander Holm <alex.holm@live.com>
2025-10-03 15:36:25 +0200

    New button icons

--- enhanced-slider.js
+++ enhanced-slider.js
@@ -263,7 +263,7 @@ class EnhancedSlider extends HTMLElement{
         style.sheet.insertRule(`:host {
             display: grid;
             grid-template-columns: auto 1fr auto;
-            column-gap: 2px;
+            column-gap: 3px;
             min-width: 10rem;
             max-width: 24rem;
             box-sizing: content-box;
@@ -336,13 +336,13 @@ class EnhancedSlider extends HTMLElement{
             margin-bottom: 0px;
         }`)
         style.sheet.insertRule(`button {
-            --size: 0.8em;
-            font-size: 1em;
+            --size: 1.25em;
+            font-size: 1rem;
             width: var(--size);
             height: var(--size);
             aspect-ratio: 1;
             box-sizing: content-box;
-            padding: 6px;
+            padding: 2px;
             margin: auto;
             border: 0px;
             border-radius: 4px;
@@ -398,26 +398,17 @@ class ButtonIntervalWrapper{
         this.intervalIndex = 0
     }
 }
-
+// https://www.svgrepo.com/collection/humbleicons-oval-line-icons/2?search=chevron
+// MIT License
 const icons = {
     left: `
-        <svg viewBox="-5 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
-            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-                <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-421.000000, -1195.000000)" fill="currentColor">
-                    <path d="M423.429,1206.98 L434.686,1196.7 C435.079,1196.31 435.079,1195.67 434.686,1195.28 C434.293,1194.89 433.655,1194.89 433.263,1195.28 L421.282,1206.22 C421.073,1206.43 420.983,1206.71 420.998,1206.98 C420.983,1207.26 421.073,1207.54 421.282,1207.75 L433.263,1218.69 C433.655,1219.08 434.293,1219.08 434.686,1218.69 C435.079,1218.29 435.079,1217.66 434.686,1217.27 L423.429,1206.98" id="chevron-left" sketch:type="MSShapeGroup">
-                    </path>
-                </g>
-            </g>
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l-7 7 7 7"/>
         </svg>
     `,
     right: `
-        <svg viewBox="-5 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns">
-            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
-                <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-473.000000, -1195.000000)" fill="currentColor">
-                    <path d="M486.717,1206.22 L474.71,1195.28 C474.316,1194.89 473.678,1194.89 473.283,1195.28 C472.89,1195.67 472.89,1196.31 473.283,1196.7 L484.566,1206.98 L473.283,1217.27 C472.89,1217.66 472.89,1218.29 473.283,1218.69 C473.678,1219.08 474.316,1219.08 474.71,1218.69 L486.717,1207.75 C486.927,1207.54 487.017,1207.26 487.003,1206.98 C487.017,1206.71 486.927,1206.43 486.717,1206.22" id="chevron-right" sketch:type="MSShapeGroup">
-                    </path>
-                </g>
-            </g>
+       <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5l7 7-7 7"/>
         </svg>
     `,
 }
---
+++ readme.md
@@ -0,0 +1,77 @@
+# enhanced-slider
+
+A web component that combines the functionality of `<input type="range">` and `<input type="number">`.
+
+[Try it on the demo page](https://alexander-holm.github.io/enhanced-slider/)
+
+## Attributes and properties
+
+Attributes are set on the component in HTML:
+```html
+<enhanced-slider min="2" max="10"></enhanced-slider>
+```
+They are linked to the underlying properties with the same name and can be accessed through Javascript. The properties can be set using either strings or numbers, but reading them will always return a string. The exception is `disabled` which is a boolean.
+```javascript
+const slider = document.querySelector("enhanced-slider")
+slider.value = 5
+console.log(slider.value) // "5"
+```
+
++ step
+    + Default: 1
+    + The interval between allowed numbers
+    + Has to be a number, a string value of "any" is not allowed
+    + If you prefer the `value` to be displayed with a certain amount of decimals you can add them in a string `step="1.0"`
++ min
+    + Default: 0
++ max
+    + Default: 100
+    + If it cannot be reached exactly with steps starting from the `min` value, it will get rounded to a number that can
++ value
+    + Default: halfway between min and max
+    + If it cannot be reached exactly with steps starting from the `min` value, it will get rounded to a number that can
+    + Has validation and does not allow invalid values like strings or out of bounds numbers
+    + If `min`, `max`, or `step` contains decimals, `value` will always have decimals as well.
+        ```javascript
+        slider.step = 0.25
+        slider.value = 5
+        console.log(slider.value) // "5.00"
+        ```
+    + Like a native `<input>` element, the underlying `value` property does not reflect its value back to the attribute
++ disabled
+    + If this attribute is present (even with the value "false") the component will be disabled
+    + Only user iteractions are disabled, the component can still be changed with Javascript
+
+## Style
+
+There are three ways to change the style:
+1. Regular CSS applied to the entire component
+2. Selecting individual parts with the `::part()` selector
+3. Changing icons with `<slot>` elements
+
+### CSS selecting the entire component
+
+The component can be selected like any other HTML element using the tag name, a class, or an ID.
+
+```css
+enhanced-slider {
+    max-width: 500px;
+    margin-inline: auto;
+    accent-color: purple;
+}
+```
+
+### Individual parts using the `::part()` selector
+
+```css
+/* Applies to both buttons */
+enhanced-slider::part(button){ color: white; }
+/* Individual buttons */
+enhanced-slider::part(increment){ background-color: red; }
+enhanced-slider::part(decrement){ background-color: blue; }
+```
+
+#### Available parts
+
+
+
